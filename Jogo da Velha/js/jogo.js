39582742aef322b78a68a20e6d096ebb640be1cf
PedroH123 <pedro_3ano@gmail.com>
2019-06-04 16:38:55 -0300

    Quadrantes Alterados para botões

---
+++ Jogo da Velha/js/jogo.js
@@ -0,0 +1,18 @@
+
+var simbolo;
+
+$(document).on("click","#X",function(){
+
+    simbolo = 1;
+
+});
+
+$(document).on("click","#O",function(){
+
+    simbolo = 2;
+
+});
+
+$(document).on("click",".casa1",function(){
+  
+})
