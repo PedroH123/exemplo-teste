db4027d3ed3c627d84b184edd47713eb9a5813b9
PedroH123 <pedro_3ano@hotmail.com>
2019-06-11 13:52:21 -0300

    Jogo Funcionando

--- Jogo da Velha/js/jogo.js
+++ Jogo da Velha/js/jogo.js
@@ -1,26 +1,105 @@
 
-var simbolo;
+var vez = "x";
+var valor = 0;
+var jogadas = 0;
 
-$(document).on("click","#X",function(){
-
-    simbolo = 1;
-
-});
-
-$(document).on("click","#O",function(){
-
-    simbolo = 2;
-
-});
+$(document).on('click','#resetar',function(){
+    $("#botao1").css("background-image", "url()");
+    $("#botao2").css("background-image", "url()");
+    $("#botao3").css("background-image", "url()");
+    $("#botao4").css("background-image", "url()");
+    $("#botao5").css("background-image", "url()");
+    $("#botao6").css("background-image", "url()");
+    $("#botao7").css("background-image", "url()");
+    $("#botao8").css("background-image", "url()");
+    $("#botao9").css("background-image", "url()");
+    $("#botao1").val("");
+    $("#botao2").val("");
+    $("#botao3").val("");
+    $("#botao4").val("");
+    $("#botao5").val("");
+    $("#botao6").val("");
+    $("#botao7").val("");
+    $("#botao8").val("");
+    $("#botao9").val("");
+    jogadas = 0;
+    habilitar();
+})
 
 $(document).on("click",".casa",function(){
 
-  if(simbolo == 1){
-    $(".casa").val() = "X";
-  }
+var valor = $(this).val();
+if(vez == "x" && valor == ""){
+  $(this).css("background-image", "url(imagens/x.jpg)");
 
-  if(simbolo == 2){
-    $(".casa").val() = "O";
-  }
+  $(this).val("x");
 
+  vez = "o";
+  jogadas++;
+
+}
+
+else if(vez == "o" && valor == ""){
+  $(this).css("background-image", "url(imagens/o.jpg)");
+
+  $(this).val("o");
+
+  vez = "x";
+  jogadas++;
+}
+
+if($("#botao1").val() == "x" && $("#botao2").val() == "x" && $("#botao3").val() == "x" ||
+        $("#botao4").val() == "x" && $("#botao5").val() == "x" && $("#botao6").val() == "x" ||
+        $("#botao7").val() == "x" && $("#botao8").val() == "x" && $("#botao9").val() == "x" ||
+        $("#botao1").val() == "x" && $("#botao4").val() == "x" && $("#botao7").val() == "x" ||
+        $("#botao2").val() == "x" && $("#botao5").val() == "x" && $("#botao8").val() == "x" ||
+        $("#botao3").val() == "x" && $("#botao6").val() == "x" && $("#botao9").val() == "x" ||
+        $("#botao1").val() == "x" && $("#botao5").val() == "x" && $("#botao9").val() == "x" ||
+        $("#botao3").val() == "x" && $("#botao5").val() == "x" && $("#botao7").val() == "x"){
+
+            alert("X venceu");
+            desabilitar();
+
+        }
+
+      else if($("#botao1").val() == "o" && $("#botao2").val() == "o" && $("#botao3").val() == "o" ||
+                $("#botao4").val() == "o" && $("#botao5").val() == "o" && $("#botao6").val() == "o" ||
+                $("#botao7").val() == "o" && $("#botao8").val() == "o" && $("#botao9").val() == "o" ||
+                $("#botao1").val() == "o" && $("#botao4").val() == "o" && $("#botao7").val() == "o" ||
+                $("#botao2").val() == "o" && $("#botao5").val() == "o" && $("#botao8").val() == "o" ||
+                $("#botao3").val() == "o" && $("#botao6").val() == "o" && $("#botao9").val() == "o" ||
+                $("#botao1").val() == "o" && $("#botao5").val() == "o" && $("#botao9").val() == "o" ||
+                $("#botao3").val() == "o" && $("#botao5").val() == "o" && $("#botao7").val() == "o"){
+
+                    alert("O venceu");
+                    desabilitar();
+                }
+
+      else if(jogadas == 9){
+        alert("Velha");
+      }
 });
+
+function desabilitar(){
+        $("#btn1").prop("disabled",true);
+        $("#btn2").prop("disabled",true);
+        $("#btn3").prop("disabled",true);
+        $("#btn4").prop("disabled",true);
+        $("#btn5").prop("disabled",true);
+        $("#btn6").prop("disabled",true);
+        $("#btn7").prop("disabled",true);
+        $("#btn8").prop("disabled",true);
+        $("#btn9").prop("disabled",true);
+    }
+
+    function habilitar(){
+        $("#btn1").prop("disabled",false);
+        $("#btn2").prop("disabled",false);
+        $("#btn3").prop("disabled",false);
+        $("#btn4").prop("disabled",false);
+        $("#btn5").prop("disabled",false);
+        $("#btn6").prop("disabled",false);
+        $("#btn7").prop("disabled",false);
+        $("#btn8").prop("disabled",false);
+        $("#btn9").prop("disabled",false);
+    }
